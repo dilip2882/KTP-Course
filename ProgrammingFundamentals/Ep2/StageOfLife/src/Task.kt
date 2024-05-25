542b16b43c4b3efbf0605af685e7328fc77410aa
Dilip Patel <pateldilip2902@gmail.com>
2024-05-26 02:36:57 +0530

    stageOfLife using when

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,16 +1,22 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    if (age >= 60) {
-        return "Old age"
-    } else if (age >= 18) {
-        return "Adulthood"
-    } else if (age >= 12) {
-        return "Teenage"
-    } else if (age >= 3) {
-        return "Childhood"
-    } else {
-        return "Infancy"
+    when {
+        age >= 60 -> {
+            return "Old age"
+        }
+        age >= 18 -> {
+            return "Adulthood"
+        }
+        age >= 12 -> {
+            return "Teenage"
+        }
+        age >= 3 -> {
+            return "Childhood"
+        }
+        else -> {
+            return "Infancy"
+        }
     }
 }
 
