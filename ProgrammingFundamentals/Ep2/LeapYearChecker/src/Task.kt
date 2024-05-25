63bf465daa5a229804766ff6af5a7d338996044b
Dilip Patel <pateldilip2902@gmail.com>
2024-05-26 02:27:03 +0530

    isLeapYear

--- ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
+++ ProgrammingFundamentals/Ep2/LeapYearChecker/src/Task.kt
@@ -1,7 +1,8 @@
 import com.faangx.ktp.basics.LeapYearCheckerMiniApp
 
 fun isLeapYear(year: Int): Boolean {
-    TODO()
+    return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+
 }
 
 fun main() {
