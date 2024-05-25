9316d044f8eae7b5b53945ff08998c81831e49ef
Dilip Patel <pateldilip2902@gmail.com>
2024-05-26 02:34:57 +0530

    stageOfLife

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,17 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    if (age >= 60) {
+        return "Old age"
+    } else if (age >= 18) {
+        return "Adulthood"
+    } else if (age >= 12) {
+        return "Teenage"
+    } else if (age >= 3) {
+        return "Childhood"
+    } else {
+        return "Infancy"
+    }
 }
 
 fun main() {
