ff0937978b2cb3e30463ace72ef21dfbe5c4c0ef
Dilip Patel <pateldilip2902@gmail.com>
2024-05-29 15:08:11 +0530

    Pattern BL

--- ProgrammingFundamentals/Ep3/Pattern_BL/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_BL/src/Task.kt
@@ -1,7 +1,18 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char: Char) {
-    TODO()
+    for (i in 1..lines) {
+        repeat(i) {
+            print(char)
+        }
+        println()
+    }
+  /*  repeat(lines) {
+        repeat(it + 1) {
+            print(char)
+        }
+        println()
+    }*/
 }
 
 fun main() {
