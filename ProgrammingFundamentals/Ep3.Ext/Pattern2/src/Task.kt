fc0b0dd782bd0319ac93e652c2f8c1c22b836a0b
Dilip Patel <pateldilip2902@gmail.com>
2024-05-29 18:07:31 +0530

    pattern 2

--- ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
@@ -1,7 +1,16 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    for (i in 1..lines) {
+        for (j in 1..lines) {
+            if (j == lines - i + 1) {
+                print("*")
+            } else {
+                print("-")
+            }
+        }
+        println()
+    }
 }
 
 fun main() {
