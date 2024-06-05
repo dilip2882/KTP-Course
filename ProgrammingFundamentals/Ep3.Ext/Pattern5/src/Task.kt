fd247a889a7ef85e02a780f470304e678c5a53c0
Dilip Patel <pateldilip2902@gmail.com>
2024-06-05 10:23:31 +0530

    Pattern 5

--- ProgrammingFundamentals/Ep3.Ext/Pattern5/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern5/src/Task.kt
@@ -1,7 +1,36 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    for (i in 0 until lines / 2) {
+        for (j in 0 until lines) {
+            if (j == i || j == lines - i - 1) {
+                print(char2)
+            } else if (j == lines / 2) {
+                print(char1)
+            } else {
+                print(' ')
+            }
+        }
+        println()
+    }
+
+    repeat(lines) {
+        print(char1)
+    }
+    println()
+
+    for (i in 0 until lines / 2) {
+        for (j in 0 until lines) {
+            if (j == (lines / 2 - 1 - i) || j == (lines / 2 + i + 1)) {
+                print(char2)
+            } else if (j == lines / 2) {
+                print(char1)
+            } else {
+                print(' ')
+            }
+        }
+        println()
+    }
 }
 
 fun main() {
