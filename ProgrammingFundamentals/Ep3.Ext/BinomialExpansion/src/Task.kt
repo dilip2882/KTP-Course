2d5ae4e970734893d7ab9dc7b249a0020054825a
Dilip Patel <pateldilip2902@gmail.com>
2024-06-05 10:31:43 +0530

    Binomial Expansion

--- ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
@@ -1,19 +1,43 @@
 import com.faangx.ktp.ext.BinomialExpansionMiniApp
 
 fun printBinomialExpansion(n: Int) {
-    TODO()
+    for (k in 0..n) {
+        val coefficient = combinationsOf(n, k)
+        val powerA = n - k
+        val powerB = k
+
+        if (coefficient > 1) print("$coefficient")
+
+        if (powerA > 0) {
+            print("a")
+            if (powerA > 1) print("^$powerA")
+        }
+        if (powerB > 0) {
+            print("b")
+            if (powerB > 1) print("^$powerB")
+        }
+        if (k < n) print(" + ")
+    }
 }
 
 fun factorialOf(x: Int): Int {
-    TODO()
+    var fact = 1
+    for (i in 1..x) {
+        fact *= i
+    }
+    return fact
 }
 
 fun permutationsOf(n: Int, r: Int): Int {
-    TODO()
+    var permutation = 1
+    for (i in (n - r + 1)..n) {
+        permutation *= i
+    }
+    return permutation
 }
 
 fun combinationsOf(n: Int, r: Int): Int {
-    TODO()
+    return permutationsOf(n, r) / factorialOf(r)
 }
 
 fun main() {
