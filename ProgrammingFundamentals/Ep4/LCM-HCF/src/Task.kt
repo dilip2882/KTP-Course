33701fa0d0a224c2bcc8309c11e850395a552000
Dilip Patel <pateldilip2902@gmail.com>
2024-06-15 00:19:22 +0530

    LCM & HCM

--- ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
+++ ProgrammingFundamentals/Ep4/LCM-HCF/src/Task.kt
@@ -1,11 +1,28 @@
 import com.faangx.ktp.basics.LcmHcfCalculatorMiniApp
 
 fun getLCM(x: Int, y: Int): Int {
-    TODO()
+    val a = if (x > y) x else y
+    val b = if (x < y) x else y
+
+    var mul = a
+    while (mul % b != 0) {
+        mul += a
+    }
+    return  mul
+
 }
 
 fun getHCF(x: Int, y: Int): Int {
-    TODO()
+    var a = if (x > y) x else y
+    var b = if (x < y) x else y
+
+    while (b != 0) {
+        val rem = a % b
+        a = b
+        b = rem
+    }
+    return a
+
 }
 
 fun main() {
